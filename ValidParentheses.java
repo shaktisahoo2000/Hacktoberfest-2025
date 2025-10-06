31fd2177a619243f34d29a2b69981ae99a1b2b16
Shakti Prasad Sahoo <75657297+shaktisahoo2000@users.noreply.github.com>
2025-10-06 13:44:44 +0530

    ValidParentheses.java
    ValidParentheses leetcode solution

---
+++ ValidParentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stk = new Stack<>();
+        
+        for(char c : s.toCharArray()){
+            if(c == '(' || c == '[' || c == '{') stk.push(c);
+            else{
+                switch(c){
+                    case ')': if(stk.isEmpty() || stk.pop() != '(') return false;
+                        break;
+                    case ']': if(stk.isEmpty() || stk.pop() != '[') return false;
+                        break;
+                    case '}': if(stk.isEmpty() || stk.pop() != '{') return false;
+                        break;
+                }
+            }
+        }
+        if(stk.isEmpty()) return true;
+        return false;
+    }
+}
